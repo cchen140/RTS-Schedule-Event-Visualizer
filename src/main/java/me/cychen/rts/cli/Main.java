9195aff5f130144433d7ab00a3bc0228efe6bbe0
CY Chen <cchen140@illinois.edu>
2017-03-14 22:51:52 -0500

    Update project to adopt a Maven framework.

--- src/main/java/me/cychen/rts/cli/Main.java
+++ src/main/java/me/cychen/rts/cli/Main.java
@@ -5,6 +5,8 @@ package me.cychen.rts.cli;
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("test");
+        System.out.println("Test starts.");
+
+
     }
 }
--- src/main/java/me/cychen/rts/gui/Main.java
+++ src/main/java/me/cychen/rts/gui/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("MainScene.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/MainScene.fxml"));
         primaryStage.setTitle("Runtime Event Monitor");
         primaryStage.setScene(new Scene(root));
         primaryStage.sizeToScene();
