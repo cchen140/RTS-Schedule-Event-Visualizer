630eb67594fcd1833dd5364cd4ee260833d5a5a9
CY Chen <cchen140@illinois.edu>
2017-03-14 23:17:59 -0500

    Add log4j2 configuration file.

--- src/main/java/me/cychen/rts/cli/Main.java
+++ src/main/java/me/cychen/rts/cli/Main.java
@@ -1,11 +1,16 @@
 package me.cychen.rts.cli;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 /**
  * Created by jjs on 2/13/17.
  */
 public class Main {
+    private static final Logger logger = LogManager.getLogger("Main");
+
     public static void main(String[] args) {
-        System.out.println("Test starts.");
+        logger.info("Hello, World!");
 
 
     }
